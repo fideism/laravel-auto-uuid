dddeadc7547b755525a19feb255dfd5acfa3b7da
fideism <nignog@126.com>
2018-10-24 17:53:12 +0800

    auto create uuid

--- src/DatabaseUuidServiceProvider.php
+++ src/DatabaseUuidServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Fideism\DatabaseUuid;
 
+use Ramsey\Uuid\Uuid;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseUuidServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,48 @@ class DatabaseUuidServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //creating
+        //booting
+        Event::listen(['eloquent.creating:*'], function ($event, $models) {
+            foreach ($models as &$model) {
+                $this->makeUuid($model);
+            }
+        });
+    }
+
+    /**
+     * @param Model $model
+     */
+    protected function makeUuid(Model &$model)
+    {
+        //keyType not uuid
+        if (! $model->getKeyType() === 'uuid') {
+            return;
+        }
+
+        //exists set uuid
+        if ($this->isUuid($model->getKey())) {
+            return;
+        }
+
+        $model->setAttribute($model->getKeyName(), $this->uuid());
+    }
+
+    /**
+     * @return string
+     */
+    protected function uuid()
+    {
+        return Uuid::uuid4()->toString();
+    }
+
+    /**
+     * @param $string
+     * @return bool
+     */
+    protected function isUuid($string)
+    {
+        return Uuid::isValid($string);
     }
 
     /**
@@ -23,6 +67,7 @@ class DatabaseUuidServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
     }
+
 }
